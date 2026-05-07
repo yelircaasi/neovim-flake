3061da98521080bcee85aa1be1838f3a33580b39
yelircaasi <yelircaasi@proton.me>
2026-05-07 23:43:26 +0200

    remove unnecessary packages and apply stylua in transpilation.nix

--- transpilation.nix
+++ transpilation.nix
@@ -5,12 +5,10 @@ pkgs.stdenv.mkDerivation {
 
   dontInstall = true;
 
-  nativeBuildInputs = with pkgs; [
-    lua5_1
-    lua51Packages.tl
-    lua51Packages.cyan
-    stylua
-  ];
+#   nativeBuildInputs = with pkgs; [
+#     lua51Packages.cyan
+#     stylua
+#   ];
 
   buildPhase = ''
     set -euo pipefail
@@ -25,43 +23,7 @@ pkgs.stdenv.mkDerivation {
         -s src \
         -b $out/config/ \
         --prune
+
+    ${pkgs.stylua}/bin/stylua $out/config
   '';
 }
-/*
-
-def transpile_tl(cfg: Config) -> None:
-    paths = cfg.paths
-    tl_root = paths.tl_dir
-    transpile_target = paths.tl_build_dir
-    copy_target = paths.config_destination
-    cyan_command: CommandList = [
-        "cyan",
-        "--gen-target",
-        "5.1",
-        "--global-env-def",
-        "vim",
-        "--global-env-def",
-        "cfg",
-        "-s",
-        paths.tl_src_dir.relative_to(tl_root).name,
-        "-b",
-        transpile_target.relative_to(tl_root).name,
-        "build",
-        "--prune",
-    ]
-    run_with_result(cyan_command, cwd=tl_root, fail_on_error=True, print_output=True)
-    result, _ = run_with_result(
-        ["stylua", transpile_target / "init.lua", transpile_target / "lua"],
-        print_output=True,
-    )
-    if result:
-        run_with_result(
-            ["lua", str(paths.cleanup_lua), transpile_target],
-            fail_on_error=True,
-            print_output=True,
-        )
-    shutil.copytree(copy_target, paths.backup_dir / copy_target.name)
-    shutil.copytree(transpile_target, copy_target, dirs_exist_ok=True)
-    print("Built lua config.")
-*/
-
